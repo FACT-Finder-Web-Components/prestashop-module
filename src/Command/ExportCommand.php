60d17ba9d5b9b5fcc5e92793d89803ae8eae0afd
Francesco Marangi <francesco.marangi@omikron.net>
2019-05-22 18:23:06 +0200

    Code review

--- src/Command/ExportCommand.php
+++ src/Command/ExportCommand.php
@@ -2,14 +2,13 @@
 
 namespace Omikron\Factfinder\Prestashop\Command;
 
-use Omikron\Factfinder\Prestashop\Api\PushImport;
 use Omikron\Factfinder\Prestashop\Config\CommunicationParams;
 use Omikron\Factfinder\Prestashop\Config\FtpParams;
 use Omikron\Factfinder\Prestashop\Export\Output\Csv;
 use Omikron\Factfinder\Prestashop\Model\Cron\EmulateEnvironment;
+use Omikron\Factfinder\Prestashop\Model\Export\Catalog\NameProvider;
 use Omikron\Factfinder\Prestashop\Model\Export\FileWriter;
 use Omikron\Factfinder\Prestashop\Model\Export\Ftp;
-use Omikron\Factfinder\Prestashop\Model\Export\NameProvider;
 use PrestaShop\PrestaShop\Core\Export\ExportDirectory;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Helper\ProgressBar;
@@ -19,11 +18,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ExportCommand extends ContainerAwareCommand
 {
-    const PUSH_IMPORT_STEP       = 'import-file';
-    const SAVE_FILE_LOCALLY_STEP = 'save-file';
-    const SHOP_ID_OPTION         = 'shop-id';
-    const LANGUAGE_ID_OPTION     = 'language-id';
-    const ADMIN_DIR_OPTION       = 'admin-dir';
+    const UPLOAD_FEED = 'upload';
+    const SAVE_FEED   = 'save';
+    const SHOP_ID     = 'shop';
+    const LANGUAGE_ID = 'language';
+    const ADMIN_DIR   = 'admin-dir';
 
     /** @var Csv */
     private $exportCsv;
@@ -34,26 +33,8 @@ class ExportCommand extends ContainerAwareCommand
     /** @var EmulateEnvironment */
     private $emulateEnv;
 
-    /** @var PushImport */
-    private $pushImport;
-
     /** @var FileWriter */
     private $fileWriter;
-
-    /** @var ProgressBar */
-    private $progressBar;
-
-    /** @var OutputInterface */
-    private $output;
-
-    /** @var InputInterface */
-    private $input;
-
-    /** @var array */
-    private $params;
-
-    /** @var array */
-    private $steps;
 
     public function __construct(
         Csv $exportCsv,
@@ -61,7 +42,7 @@ class ExportCommand extends ContainerAwareCommand
         EmulateEnvironment $emulateEnvironment,
         FileWriter $fileWriter
     ) {
-        parent::__construct('fact-finder:feed');
+        parent::__construct('factfinder:feed');
         $this->exportCsv       = $exportCsv;
         $this->exportDirectory = $exportDirectory;
         $this->emulateEnv      = $emulateEnvironment;
@@ -70,84 +51,78 @@ class ExportCommand extends ContainerAwareCommand
 
     public function configure()
     {
-        $this->setName('fact-finder:feed')
-            ->setDescription('Export feed file to selected directory')
-            ->addOption(self::SHOP_ID_OPTION, 'shop' ,InputOption::VALUE_REQUIRED,'Sets the shop identifier used in environment emulation', 1)
-            ->addOption(self::LANGUAGE_ID_OPTION, 'lang', InputOption::VALUE_REQUIRED, 'Sets the language identifier used in environment emulation,', 1)
-            ->addOption(self::PUSH_IMPORT_STEP, 'import', InputOption::VALUE_REQUIRED, 'Determines if feed file should be uploaded to configured FACT-Finder and then, imported', true)
-            ->addOption(self::SAVE_FILE_LOCALLY_STEP, 'save', InputOption::VALUE_REQUIRED, 'Determines if feed file should be saved locally', false)
-            ->addOption(self::ADMIN_DIR_OPTION , 'admin', InputOption::VALUE_REQUIRED, 'Admin directory, required to correctly store feed file', 'admin');
+        $this->setDescription('Export feed file to selected directory');
+        $this->addOption(self::SHOP_ID, 's', InputOption::VALUE_OPTIONAL, 'Shop ID of the shop to be exported', 1);
+        $this->addOption(self::LANGUAGE_ID, 'l', InputOption::VALUE_OPTIONAL, 'Requested language ID', 1);
+        $this->addOption(self::UPLOAD_FEED, 'u', InputOption::VALUE_NONE, 'Upload feed to the FACT-Finder server');
+        $this->addOption(self::SAVE_FEED, 'f', InputOption::VALUE_NONE, 'Save feed locally');
+        $this->addOption(self::ADMIN_DIR, null, InputOption::VALUE_OPTIONAL, 'Admin directory location', 'admin');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->init($input, $output);
-        $this->params = array_merge((new CommunicationParams())->getParameters(), (new FtpParams())->getParameters());
-        $fileName     =  (new NameProvider())->getName() ;
-        $filePath     = $this->exportDirectory . $fileName;
-        $handle       = $this->exportCsv->write();
-        $this->advance('Feed file has been generated');
+        $progress = $this->progress($output, $input->getOption(self::UPLOAD_FEED) + $input->getOption(self::SAVE_FEED));
 
-        if ($this->steps[self::PUSH_IMPORT_STEP]) {
+        $fileName = (new NameProvider())->getName();
+        $handle   = $this->exportCsv->write();
+
+        $progress('Feed file has been generated');
+
+        if ($input->getOption(self::UPLOAD_FEED)) {
             $ftp = new Ftp($fileName);
-            $ftp->open(new FtpParams())->upload($handle)->close();
-            $this->pushImport->execute();
-            $this->advance(sprintf('Feed file uploaded to %s successfully and import has been started on %s', $this->params['host'], $this->params['url']));
+            $ftp->open(new FtpParams());
+            $ftp->upload($handle);
+            $ftp->close();
+
+            $this->getContainer()->get('factfinder.api_push_import')->execute();
+
+            $serverUrl = (new CommunicationParams())->getServerUrl(false);
+            $progress('Feed file uploaded successfully and import has been started on %s', $serverUrl);
         }
 
-        if ($this->steps[self::SAVE_FILE_LOCALLY_STEP]) {
+        if ($input->getOption(self::SAVE_FEED)) {
+            $filePath = $this->exportDirectory . $fileName;
             $this->fileWriter->save($handle, $filePath);
-            $this->advance(sprintf('Feed file saved in %s successfully', $filePath));
+            $progress('Feed file successfully saved in %s', $filePath);
         }
-        return true;
     }
 
-    private function init(InputInterface $input, OutputInterface $output)
+    private function progress(OutputInterface $output, $max)
     {
-        $this->input = $input;
-        $this->output = $output;
+        if ($output->getVerbosity() < OutputInterface::VERBOSITY_VERBOSE) {
+            return function () {
+            };
+        }
+
+        $progressBar = new ProgressBar($output);
+        $progressBar->setFormat("<info>%message%</info>\n" . $progressBar->getFormatDefinition('normal'));
+        $progressBar->setMessage('Generating export feed...');
+        $progressBar->start($max + 1);
+
+        return function ($message, ...$args) use ($progressBar, $output) {
+            $progressBar->setMessage(vsprintf($message, $args));
+            $progressBar->advance();
+            if ($progressBar->getMaxSteps() === $progressBar->getProgress()) {
+                $progressBar->finish();
+                $output->writeln('');
+            }
+        };
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        require dirname($this->getContainer()->get('kernel')->getRootDir()) . '/config/config.inc.php';
+
+        $adminDir = _PS_ROOT_DIR_ . DIRECTORY_SEPARATOR . $input->getOption(self::ADMIN_DIR);
+        if (!is_dir($adminDir)) {
+            throw new \Exception(sprintf('%s is not a valid directory', $adminDir));
+        }
+        define('_PS_ADMIN_DIR_', $adminDir);
+
         $this->emulateEnv->emulate(
-            $input->getOption(self::SHOP_ID_OPTION),
-            $input->getOption(self::LANGUAGE_ID_OPTION),
-            $input->getOption(self::ADMIN_DIR_OPTION)
+            \Context::getContext(),
+            $input->getOption(self::SHOP_ID),
+            $input->getOption(self::LANGUAGE_ID)
         );
-        $this->pushImport  = $this->getContainer()->get('factfinder.api_push_import');
-
-        $this->progressBar = new ProgressBar($output, $this->countSteps());
-        $this->progressBar->setFormat(sprintf('%s <info>%%message%%</info>', $this->progressBar->getFormatDefinition('normal')));
-        $this->progressBar->setBarCharacter('<fg=green>⚬</>');
-        $this->progressBar->setEmptyBarCharacter("<fg=red>⚬</>");
-        $this->progressBar->setProgressCharacter("<fg=green>➤</>");
-    }
-
-    private function countSteps()
-    {
-        return array_reduce([self::PUSH_IMPORT_STEP, self::SAVE_FILE_LOCALLY_STEP], function ($counter, $step) {
-                $this->steps[$step] = $this->checkOptionIsTrue($step);
-                return $this->steps[$step] ? ++$counter : $counter;
-            }) + 1;
-    }
-
-    /**
-     * @param string $optionName
-     *
-     * @return bool
-     */
-    private function checkOptionIsTrue($optionName)
-    {
-        return filter_var($this->input->getOption($optionName), FILTER_VALIDATE_BOOLEAN);
-    }
-
-    /**
-     * @param string   $message
-     *
-     */
-    private function advance($message)
-    {
-        if ($this->output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
-            $this->progressBar->setMessage($message, 'message');
-            $this->progressBar->advance();
-            $this->output->writeln('');
-        }
     }
 }
--- src/Model/Cron/EmulateEnvironment.php
+++ src/Model/Cron/EmulateEnvironment.php
@@ -2,40 +2,23 @@
 
 namespace Omikron\Factfinder\Prestashop\Model\Cron;
 
-use Symfony\Component\HttpKernel\Kernel;
-
 class EmulateEnvironment
 {
-    /** @var Kernel */
-    private $kernel;
-
-    public function __construct(Kernel $kernel)
-    {
-        $this->kernel = $kernel;
-    }
-
     /**
      * This method is used for initialize partially mocked PrestaShop environment
      * causing its services will be able to use from CLI script
      *
-     * @param string $idShop
-     * @param string $idLang
-     * @param string $adminPath
+     * @param \Context $context
+     * @param string   $idShop
+     * @param string   $idLang
      *
      * @throws \Exception
      */
-    public function emulate($idShop, $idLang, $adminPath)
+    public function emulate(\Context $context, $idShop, $idLang)
     {
-        require $this->kernel->getRootDir() . '/../config/config.inc.php';
-        \Context::getContext()->language   = new \Language($idLang);
-        \Context::getContext()->shop       = new \Shop($idShop, $idLang);
-        \Context::getContext()->employee   = new \Employee(null, $idLang, $idShop);
-        \Context::getContext()->controller = new \AdminControllerCore('Admin');
-        $adminPath                         = _PS_ROOT_DIR_ . DIRECTORY_SEPARATOR . $adminPath;
-        if (is_dir($adminPath)) {
-            define('_PS_ADMIN_DIR_', $adminPath);
-        } else {
-            throw new \Exception(sprintf('Specified path for admin directory %s is not exists', $adminPath));
-        }
+        $context->language   = new \Language($idLang);
+        $context->shop       = new \Shop($idShop, $idLang);
+        $context->employee   = new \Employee(null, $idLang, $idShop);
+        $context->controller = new \AdminControllerCore('Admin');
     }
 }
